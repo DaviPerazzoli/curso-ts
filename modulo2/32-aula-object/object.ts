6737199310dc621d03fe56696e1411422ef91cba
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-03-24 11:41:07 -0300

    aprendi objects

--- modulo2/32-aula-object/object.ts
+++ modulo2/32-aula-object/object.ts
@@ -83,3 +83,100 @@ function onboarding04(pessoa: Pessoa04){
 }
 console.log(onboarding04({ nome: 'Davi', funcao: 'estudante', linguagem: 'Javascript'}));
 
+// propriedade 'readonly' (para que os devs não modifiquem a propriedade)
+
+interface Pessoa05 {
+    nome: string;
+    funcao: string;
+    linguagem: string;
+    readonly email: string;
+}
+
+function onboarding05(pessoa: Pessoa05){
+    return (
+        'Seja bem vindo ' +
+        pessoa.nome +
+        '! ' +
+        'Sua função é ' + 
+        pessoa.funcao + 
+        '. E sua linguagem é '+
+        pessoa.linguagem +
+        '. E seu email é ' +
+        pessoa.email
+    );
+}
+console.log(onboarding05({ nome: 'Davi', funcao: 'estudante', linguagem: 'Javascript', email:'davi@gmail.com'}));
+
+// tipos de extensões (heranças)
+
+interface Mae {
+    nome: string;
+}
+
+interface Pai {
+    sobrenome: string;
+}
+
+interface Filha extends Mae, Pai {
+    idade: number;
+}
+
+const filha: Filha = {
+    nome: 'Davi',
+    sobrenome: 'Perazzoli',
+    idade: 18
+};
+
+// Tipos de interseções
+
+interface Cachorro {
+    tipo: string;
+}
+
+interface Gato {
+    tipo: string;
+}
+
+type Animal = Cachorro & Gato;
+
+const animal: Animal = {
+    tipo: ''
+}
+
+// Generic Objects
+
+type Usuario = {
+    nome: string;
+    email: string;
+}
+
+type Admin = {
+    nome: string;
+    email: string;
+    admin: boolean;
+}
+
+const usuario: Usuario = {
+    nome: 'Davi Perazzoli',
+    email: 'davi@gmail.com'
+};
+
+
+const admin: Admin = {
+    nome: 'Davi Perazzoli',
+    email: 'davi@gmail.com',
+    admin: true
+};
+
+function acessarSistema<T>(usuario: T): T {
+    return usuario;
+}
+
+console.log(acessarSistema<Usuario>(usuario));
+console.log(acessarSistema<Admin>(admin));
+
+// function acessarSistema(usuario: Usuario): Usuario {
+//     return usuario;
+// }
+
+// console.log(acessarSistema(usuario));
